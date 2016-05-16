ba95222e9c12e283a82bccbff21cb57c0932f8ab
nicolattes <nicolattes@172-16-118-103.DYNAPOOL.NYU.EDU>
2016-05-16 00:34:41 -0400

    updated
    made landing page clickable
    fixed z-index of header
    added fancy box to all galleries

---
+++ js/BackgroundArray.js
@@ -0,0 +1,23 @@
+// from https://codepen.io/dudleystorey/pen/qEoKzZ
+
+var bgImageArray = ["../../images/01.jpg", "../../images/02.jpg", "../../images/03.jpg", "../../images/04.jpg"],
+base = "../../images/01.jpg",
+secs = 4;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
+
